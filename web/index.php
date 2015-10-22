8acf0c7176ba000cafd0b031b25070f3d7d58c88
Elliot Wright <wright.elliot@gmail.com>
2015-10-22 14:05:40 +0100

    Requiring the Pimcore index.php file instead of using a modified version of it. Added .editorconfig file.

--- web/index.php
+++ web/index.php
@@ -13,15 +13,5 @@
  * file that was distributed with this source code.
  */
 
-include_once __DIR__ . "/../vendor/autoload.php";
-include_once __DIR__ . "/pimcore/config/startup.php";
-
-try {
-    Pimcore::run();
-} catch (Exception $e) {
-    if (class_exists("Logger")) {
-        Logger::emerg($e);
-    }
-
-    throw $e;
-}
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/pimcore/pimcore/index.php";
