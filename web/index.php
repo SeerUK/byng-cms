a74e9cbe0251f1138c081bcf61689f304068e2f4
Elliot Wright <wright.elliot@gmail.com>
2015-10-22 23:48:58 +0100

    Updated Pimcore installer. Using slightly modified Pimcore index.php. Moved vendor dir into web dir so that Pimcore automatically uses it.

--- web/index.php
+++ web/index.php
@@ -1,17 +1,13 @@
 <?php
 
-/**
- * This file is part of the byng-cms package.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+require_once __DIR__ . "/pimcore/config/startup.php";
 
-require_once __DIR__ . "/../vendor/autoload.php";
-require_once __DIR__ . "/../vendor/pimcore/pimcore/index.php";
+try {
+    Pimcore::run();
+} catch (Exception $e) {
+    if (class_exists("Logger")) {
+        Logger::emerg($e);
+    }
+
+    throw $e;
+}
