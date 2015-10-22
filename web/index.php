8e005ff517ae96446a71019ac845804ee15846fd
Elliot Wright <wright.elliot@gmail.com>
2015-10-23 00:11:50 +0100

    Updated Pimcore installer. Using custom index.php to allow composer vendor dir to be outside of web folder, not using Pimcore's automatic composer inclusion.

--- web/index.php
+++ web/index.php
@@ -1,13 +1,26 @@
 <?php
-
-require_once __DIR__ . "/pimcore/config/startup.php";
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @copyright  Copyright (c) 2009-2014 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+include_once("pimcore/config/startup.php");
 
 try {
     Pimcore::run();
+
 } catch (Exception $e) {
-    if (class_exists("Logger")) {
+    // handle exceptions, log to file
+    if(class_exists("Logger")) {
         Logger::emerg($e);
     }
-
-    throw $e;
+        throw $e;
 }
